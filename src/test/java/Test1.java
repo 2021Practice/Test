9d4448dd0dd1e741a30110c6e4d94ea192ab09b4
2021Practice <spractice2021@gmail.com>
2021-12-04 10:48:12 -0600

    first commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	@Test
+	public void Testcase1() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		driver.get("https://gmail.com");
+		System.out.println(driver.getTitle());
+		
+//		driver.navigate().to("https://amazon.ca");
+//		System.out.println(driver.getTitle());
+//		
+//		driver.navigate().to("https://gmail.com");
+//		System.out.println(driver.getTitle());
+//		
+//		driver.findElement(By.linkText("Sign in")).click();
+		driver.findElement(By.id("identifierId")).sendKeys("spractice2021@gmail.com");
+		
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]"))))).click().perform();
+		
+		driver.quit();
+	}
+	
+	
+	
+	
+}
