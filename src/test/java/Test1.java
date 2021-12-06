37722776698c970be79826d91fb2379584bc6cff
2021Practice <spractice2021@gmail.com>
2021-12-06 10:58:10 -0600

    updated java file commit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -13,6 +13,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Test1 {
 	@Test
 	public void Testcase1() {
+
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
